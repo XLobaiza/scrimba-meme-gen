b529eccede0c8bb02eaf9c1dc33ae637ae53e91d
Exequiel Lobaiza <exelob25@gmail.com>
2024-09-22 07:17:07 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className="navbar">
+            <div className="navbar-logo">
+            <img src="src/assets/Logo.svg"/>
+            <h4>Meme Generator</h4>
+            </div>
+        <p>React Course - Project 3</p>
+        </div>
+    )
+}
